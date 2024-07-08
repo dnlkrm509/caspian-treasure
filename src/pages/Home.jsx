6c284da0ffb875c82ce4ac2eb847aaafc22f7149
Daniel <danialkarimi109@gmail.com>
2024-07-08 01:38:16 +0100

    update Home animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,8 +19,10 @@ function HomePage(props) {
 
   const { scrollY } = useScroll();
 
-  const yContent = useTransform(scrollY, [550, 600, 650, 680], [-40, -150, -150, -50]);
-  const yContentP = useTransform(scrollY, [550, 600, 650, 680], [-40, -100, -100, -50]);
+  const yContent = useTransform(scrollY, [550, 600, 650, 680], [0, -150, -150, -180]);
+  const yContentP = useTransform(scrollY, [550, 600, 650, 680], [0, -100, -100, -120]);
+
+  const opacityContent = useTransform(scrollY, [550, 600, 650, 680], [0, 0.6, 0.7, 1]);
   const scaleContent = useTransform(scrollY, [550, 600], [1, 1.5]);
 
   useEffect(() => {
@@ -56,17 +58,17 @@ function HomePage(props) {
       <div className="container gap-8 flex flex-col">
         <Header onShowCart={props.onShowCart} />
         <div className="w-11/12 mx-auto">
-          <motion.h1 style={{ y: yContent, scale: scaleContent }} className="text-center my-[30px]">Content</motion.h1>
-          <motion.p style={{ y: yContentP }}>
+          <motion.h1 style={{ y: yContent, scale: scaleContent, opacity: opacityContent }} className="text-center my-[30px]">Content</motion.h1>
+          <motion.p style={{ y: yContentP, opacity: opacityContent }}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare venenatis felis at pulvinar. Etiam non eleifend sapien. Pellentesque nec orci vel elit consequat laoreet. Cras id fermentum turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel imperdiet purus, eget hendrerit ipsum. Duis quis commodo purus. In diam nibh, consequat ac posuere sit amet, condimentum in odio. Maecenas ultrices id ligula nec fringilla. Aenean pellentesque ac metus vitae porttitor. Vestibulum cursus aliquet magna vitae aliquam. Aenean faucibus tellus in tortor congue interdum non ut lorem. Aliquam ut ligula ac odio tincidunt tincidunt non sit amet arcu. Sed imperdiet ligula sit amet orci viverra auctor. In quam quam, cursus nec tortor ac, placerat mollis tortor.
           </motion.p>
-          <motion.p style={{ y: yContentP }}>
+          <motion.p style={{ y: yContentP, opacity: opacityContent }}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare venenatis felis at pulvinar. Etiam non eleifend sapien. Pellentesque nec orci vel elit consequat laoreet. Cras id fermentum turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel imperdiet purus, eget hendrerit ipsum. Duis quis commodo purus. In diam nibh, consequat ac posuere sit amet, condimentum in odio. Maecenas ultrices id ligula nec fringilla. Aenean pellentesque ac metus vitae porttitor. Vestibulum cursus aliquet magna vitae aliquam. Aenean faucibus tellus in tortor congue interdum non ut lorem. Aliquam ut ligula ac odio tincidunt tincidunt non sit amet arcu. Sed imperdiet ligula sit amet orci viverra auctor. In quam quam, cursus nec tortor ac, placerat mollis tortor.
           </motion.p>
-          <motion.p style={{ y: yContentP }}>
+          <motion.p style={{ y: yContentP, opacity: opacityContent }}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare venenatis felis at pulvinar. Etiam non eleifend sapien. Pellentesque nec orci vel elit consequat laoreet. Cras id fermentum turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel imperdiet purus, eget hendrerit ipsum. Duis quis commodo purus. In diam nibh, consequat ac posuere sit amet, condimentum in odio. Maecenas ultrices id ligula nec fringilla. Aenean pellentesque ac metus vitae porttitor. Vestibulum cursus aliquet magna vitae aliquam. Aenean faucibus tellus in tortor congue interdum non ut lorem. Aliquam ut ligula ac odio tincidunt tincidunt non sit amet arcu. Sed imperdiet ligula sit amet orci viverra auctor. In quam quam, cursus nec tortor ac, placerat mollis tortor.
           </motion.p>
-          <motion.p style={{ y: yContentP }}>
+          <motion.p style={{ y: yContentP, opacity: opacityContent }}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare venenatis felis at pulvinar. Etiam non eleifend sapien. Pellentesque nec orci vel elit consequat laoreet. Cras id fermentum turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel imperdiet purus, eget hendrerit ipsum. Duis quis commodo purus. In diam nibh, consequat ac posuere sit amet, condimentum in odio. Maecenas ultrices id ligula nec fringilla. Aenean pellentesque ac metus vitae porttitor. Vestibulum cursus aliquet magna vitae aliquam. Aenean faucibus tellus in tortor congue interdum non ut lorem. Aliquam ut ligula ac odio tincidunt tincidunt non sit amet arcu. Sed imperdiet ligula sit amet orci viverra auctor. In quam quam, cursus nec tortor ac, placerat mollis tortor.
           </motion.p>
         </div>
