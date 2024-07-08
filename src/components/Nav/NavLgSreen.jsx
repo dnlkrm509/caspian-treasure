2ba8b6a739ef48acbb8f2508928c3c97c7e7fcb9
Daniel <danialkarimi109@gmail.com>
2024-07-08 22:28:40 +0100

    update nav

--- src/components/Nav/NavLgSreen.jsx
+++ src/components/Nav/NavLgSreen.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import classes from './Nav.module.css';
 
 function NavLgScreen() {
     return (
--- src/components/Nav/NavSmScreen.jsx
+++ src/components/Nav/NavSmScreen.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import classes from './Nav.module.css';
 
 const NavSmScreen = () => {
     const [isShow, setIsShow] = useState(false);
