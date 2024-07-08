8ad7897b7854412f2e1ee3bab2ad9a089dd9983a
Daniel <danialkarimi109@gmail.com>
2024-07-08 21:53:29 +0100

    update nav

--- src/components/Nav/NavSmScreen.jsx
+++ src/components/Nav/NavSmScreen.jsx
@@ -2,6 +2,15 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from './Nav.module.css';
 
+const styles = {
+  open: {
+    visibility: visible,
+    height: '100%',
+    opacity: 0.9
+  },
+  active: {display: flex}
+};
+
 const NavSmScreen = () => {
     const [isShow, setIsShow] = useState(false);
     const showNavHandler = () => {
@@ -11,7 +20,7 @@ const NavSmScreen = () => {
     return (
         <header className="text-center w-full mt-[20px] mb-[70px] relative fixed top-0 left-0">
             {/* Button */}
-                <button onClick={showNavHandler} className={`${classes.btn} fixed top-[5%] right-[2%] h-[27px] w-[35px] cursor-pointer z-[100] ${isShow ? 'flex' : ''}`}>
+                <button onClick={showNavHandler} className={`${classes.btn} fixed top-[5%] right-[2%] h-[27px] w-[35px] cursor-pointer z-[100] ${isShow ?  styles.active : ''}`}>
                     <span className="top"></span>
                     <span className="middle"></span>
                     <span className="bottom"></span>
@@ -19,7 +28,7 @@ const NavSmScreen = () => {
 
 
             {/* Links */}
-            <div className={`${classes.overlay} h-full w-full fixed top-0 left-0 z-[50] ${isShow ? classes.open : ''}`}>
+            <div className={`${classes.overlay} h-full w-full invisible fixed top-0 left-0 z-[50] ${isShow ? styles.open : ''}`}>
                 <nav>
                     <ul className={`${classes.links} flex w-full justify-around ${isShow ? classes.active : ''}`}>
                         <li onClick={() => setIsShow(false)}><Link to="/">Home</Link></li>
