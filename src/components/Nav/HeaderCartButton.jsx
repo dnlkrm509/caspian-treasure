57b4249ef7f06d81dc122ba6401fcadee0afe575
Daniel <danialkarimi109@gmail.com>
2024-07-08 20:54:55 +0100

    delete unneccessary parts of the code

--- src/components/Nav/HeaderCartButton.jsx
+++ src/components/Nav/HeaderCartButton.jsx
@@ -1,40 +1,22 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { motion } from 'framer-motion';
 import CartContext from '../../store/cart-context.js';
 import CartIcon from '../Cart/CartIcon.jsx';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount
   }, 0);
 
-  const {items} = cartCtx;
-  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
-
-  useEffect(() => {
-    if(cartCtx.items.length === 0) {
-      return;
-    }
-    setBtnIsHighlighted(true);
-
-    const timer = setTimeout(() => {
-      setBtnIsHighlighted(false);
-    },300);
-
-    return () => {
-      clearTimeout(timer);
-    }
-  }, [items])
 
   return (
     <motion.button
       whileHover={{ scale: [0.8, 1.3, 1] }}
       transition={{ type: 'spring', stiffness: 500 }}
-      onClick={props.onClick} className={btnClasses}
+      onClick={props.onClick} className={classes.button}
     >
       <span className={classes.icon}>
         <CartIcon/>
