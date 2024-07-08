18cdbf7bf700b0ebbc7f81011efb71d851c335a6
Daniel <danialkarimi109@gmail.com>
2024-07-08 22:38:33 +0100

    update nav

--- src/components/Nav/NavSmScreen.jsx
+++ src/components/Nav/NavSmScreen.jsx
@@ -10,7 +10,7 @@ const NavSmScreen = () => {
     return (
         <header className="text-center w-full mt-[20px] mb-[70px] relative fixed top-0 left-0">
             {/* Button */}
-                <button onClick={showNavHandler} className={`btn hidden fixed top-[5%] right-[2%] h-[27px] w-[35px] cursor-pointer z-[100] ${isShow ?  'active' : ''}`}>
+                <button onClick={showNavHandler} className={`btn hidden fixed top-[5%] right-[2%] h-[27px] w-[35px] cursor-pointer z-[100] ${isShow ? 'active' : ''}`}>
                     <span className="top"></span>
                     <span className="middle"></span>
                     <span className="bottom"></span>
