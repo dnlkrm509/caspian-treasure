ae8dec6329a03fdfc763f8d1c9bd914ca32852d6
Daniel <danialkarimi109@gmail.com>
2024-07-08 22:03:17 +0100

    update nav

--- src/components/Nav/NavSmScreen.jsx
+++ src/components/Nav/NavSmScreen.jsx
@@ -2,15 +2,6 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from './Nav.module.css';
 
-const styles = {
-  open: {
-    visibility: visible,
-    height: '100%',
-    opacity: 0.9
-  },
-  active: {display: flex}
-};
-
 const NavSmScreen = () => {
     const [isShow, setIsShow] = useState(false);
     const showNavHandler = () => {
@@ -20,7 +11,7 @@ const NavSmScreen = () => {
     return (
         <header className="text-center w-full mt-[20px] mb-[70px] relative fixed top-0 left-0">
             {/* Button */}
-                <button onClick={showNavHandler} className={`${classes.btn} fixed top-[5%] right-[2%] h-[27px] w-[35px] cursor-pointer z-[100] ${isShow ?  styles.active : ''}`}>
+                <button onClick={showNavHandler} className={`${classes.btn} hidden fixed top-[5%] right-[2%] h-[27px] w-[35px] cursor-pointer z-[100] ${isShow ?  'active' : ''}`}>
                     <span className="top"></span>
                     <span className="middle"></span>
                     <span className="bottom"></span>
@@ -28,9 +19,9 @@ const NavSmScreen = () => {
 
 
             {/* Links */}
-            <div className={`${classes.overlay} h-full w-full invisible fixed top-0 left-0 z-[50] ${isShow ? styles.open : ''}`}>
+            <div className={`${classes.overlay} h-full w-full invisible fixed top-0 left-0 z-[50] ${isShow ? 'open' : ''}`}>
                 <nav>
-                    <ul className={`${classes.links} flex w-full justify-around ${isShow ? classes.active : ''}`}>
+                    <ul className={`${classes.links} flex w-full justify-around ${isShow ? 'active' : ''}`}>
                         <li onClick={() => setIsShow(false)}><Link to="/">Home</Link></li>
                         <li onClick={() => setIsShow(false)}><Link to="/about">About Us</Link></li>
                         <li onClick={() => setIsShow(false)}><Link to="/products">Products</Link></li>
