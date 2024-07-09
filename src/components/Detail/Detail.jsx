520f6fbe99ed62df77db4823d7920c4c9fc06657
Daniel <danialkarimi109@gmail.com>
2024-07-09 15:26:09 +0100

    update Detail.jsx component

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -34,12 +34,13 @@ function Detail ({ product }) {
 
     return (
         <motion.div initial='initial' animate='animate' exit={{ opacity: 0 }}>
-            <div className={classes.fullscreen}>
-              <div className={classes.product}>
-                  <motion.div
+            <div className={classes.product}>
+                <motion.div
                   className={classes.img}
                   animate={{ opacity: 1 }}
-                  initial={{ opacity: 0 }}>
+                  initial={{ opacity: 0 }}
+                >
+                  <span>Image</span>
                   {/* <motion.img
                       key={props.product.image}
                       src={props.product.image}
@@ -48,35 +49,34 @@ function Detail ({ product }) {
                       exit={{ opacity: 0 }}
                       transition={{ delay: 0.2 }}
                   />
-                  */}
-                  </motion.div>
-                  <div className={classes['product-details']}>
-                    <motion.div variants={stagger} className={classes.inner}>
-                      <Link to='..' relative='path'>
-                        <motion.button variants={fadeInUp}>
-                            <span className={classes['go-back']}>Back to products</span>
-                        </motion.button>
-                      </Link>
-                      <motion.div variants={fadeInUp}>
-                        <span className={classes.category}>Caviar</span>
-                      </motion.div>
-                      <motion.h1 variants={fadeInUp}>{product.name}</motion.h1>
-                      <motion.p variants={fadeInUp}>{product.description}</motion.p>
-                      <motion.div variants={fadeInUp} className={classes['qty-price']}>
-                        <div className={classes.qty}>
-                          <div className={classes.minus}>-</div>
-                          <div className={classes.amount}>1</div>
-                          <div className={classes.add}>+</div>
-                        </div>
-                        <span className={classes.price}>{product.price}</span>
-                      </motion.div>
-                      <motion.div variants={fadeInUp} className={classes['btn-row']}>
-                        <button className={classes['add-to-cart']}>Add to cart</button>
-                        <button className={classes.subscribe}>Subscribe</button>
-                      </motion.div>
+                */}
+                </motion.div>
+                <div className={classes['product-details']}>
+                  <motion.div variants={stagger} className={classes.inner}>
+                    <Link to='..' relative='path'>
+                      <motion.button variants={fadeInUp}>
+                          <span className={classes['go-back']}>Back to products</span>
+                      </motion.button>
+                    </Link>
+                    <motion.div variants={fadeInUp}>
+                      <span className={classes.category}>Caviar</span>
                     </motion.div>
-                  </div>
-              </div>
+                    <motion.h1 variants={fadeInUp}>{product.name}</motion.h1>
+                    <motion.p variants={fadeInUp}>{product.description}</motion.p>
+                    <motion.div variants={fadeInUp} className={classes['qty-price']}>
+                      <div className={classes.qty}>
+                        <div className={classes.minus}>-</div>
+                        <div className={classes.amount}>1</div>
+                        <div className={classes.add}>+</div>
+                      </div>
+                      <span className={classes.price}>{product.price}</span>
+                    </motion.div>
+                    <motion.div variants={fadeInUp} className={classes['btn-row']}>
+                      <button className={classes['add-to-cart']}>Add to cart</button>
+                      <button className={classes.subscribe}>Subscribe</button>
+                    </motion.div>
+                  </motion.div>
+                </div>
             </div>
           </motion.div>
 
