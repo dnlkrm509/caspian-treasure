fe2d2914bbef0c6f82b0d39c13ac50daf9f689e7
Daniel <danialkarimi109@gmail.com>
2024-07-10 00:05:50 +0100

    update detail

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,10 @@
 import { useParams, useLocation } from "react-router-dom";
 import Detail from "../components/Detail/Detail";
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../store/cart-context";
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 function DetailPage() {
     const { productId } = useParams();
@@ -15,6 +20,30 @@ function DetailPage() {
         description: decodeURIComponent(description),
         price
     }
+
+    const cartCTX = useContext(CartContext);
+
+    const [isFetching, setIsFetching] = useState(false);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        async function fetchCartProduct() {
+            setIsFetching(true);
+
+            try {
+                const products = await axios.get(`${apiUrl}/api/cart-products`);
+                
+                cartCTX.setCart({ items: products.data.rows, totalAmount: +products.data.rows[ products.data.rows.length -1 ].totalAmount });
+            } catch (error) {
+                setError({ message: "Failed to fetch cart products." });
+            }
+
+            setIsFetching(false);
+        }
+
+        fetchCartProduct();
+    }, [])
 
     return (
         <div>
