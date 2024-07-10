237dc13310a0eceb936086757b9a0fe3b4cf8f6b
Daniel <danialkarimi109@gmail.com>
2024-07-10 19:02:15 +0100

    implement add and remove item from cart

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -50,9 +50,10 @@ function DetailPage() {
 
     return (
         <div>
-            {isFetching && cartCTX.items.length === 0 && <LoadingSpinner />}
-            {error && <Error title='An Error occurred!' body={error} />}
-            {!isFetching && !error && cartCTX.items.length > 0 && <Detail product={product} cart={cartCTX.items} productId={productId} />}
+            {isFetching && (!cartCTX.items || cartCTX.items.length === 0) && <LoadingSpinner />}
+            {error && <Error title='An Error occurred!' body={error.message} />}
+            {!isFetching && !error && cartCTX.items && cartCTX.items.length > 0 && (
+                <Detail product={product} cart={cartCTX.items} productId={productId} />)}
         </div>
     )
 }
