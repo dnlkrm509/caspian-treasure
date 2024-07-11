ded4e7f13d0c1910e96791ce1b65849a09f6ab09
Daniel <danialkarimi109@gmail.com>
2024-07-11 15:34:15 +0100

    implement add item to cart

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -49,7 +49,7 @@ function Detail ({ product, productId }) {
     }
 
     fetchCartProductAmount();
-  }, [product.id, setNewAmount])
+  }, [product.id, response, carts, existingCartItem])
   
   const cartCtx = useContext(CartContext);
 
