7c9a640a83835043b71b8d5b72eb47328546c8aa
dnlkrm509 <109435295+dnlkrm509@users.noreply.github.com>
2024-07-11 09:19:48 +0100

    Update Detail.jsx

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -53,7 +53,7 @@ function Detail ({ product, cart, productId }) {
 
     const isExistingCartItem = item[0].product_id === +id;
 
-    let updatedTotalAmount = +item[0].totalAmount - +item[0].price;;
+    let updatedTotalAmount = +item[0].totalAmount - +item[0].price;
     
     if (item[0].length === 0) {
       updatedTotalAmount = 0;
