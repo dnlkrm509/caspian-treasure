829f8cf683097c3ed035b41838e5751f8fb5baf5
dnlkrm509 <109435295+dnlkrm509@users.noreply.github.com>
2024-07-11 08:59:55 +0100

    Update Detail.jsx

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -184,11 +184,8 @@ function Detail ({ product, cart, productId }) {
 
   let amount = <span className={classes.amount}>0</span>;
 
-  if (item.length === 0) {
-    return;
-  } else {
-    if (item[0])
-      amount = <span className={classes.amount}>{item[0].amount}</span>;
+  if (item[0])
+    amount = <span className={classes.amount}>{item[0].amount}</span>;
   }
 
     return (
