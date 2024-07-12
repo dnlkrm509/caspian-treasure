23fe90037525055cca2ed2cff9544755b49f91af
Daniel <danialkarimi109@gmail.com>
2024-07-12 21:31:14 +0100

    update router

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,4 +1,4 @@
-import { useParams, useLocation, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import Detail from "../components/Detail/Detail";
 import { useContext, useEffect, useState } from "react";
 import CartContext from "../store/cart-context";
@@ -10,57 +10,14 @@ const apiUrl = import.meta.env.VITE_API_URL;
 
 function DetailPage() {
     const data = useLoaderData();
-    console.log(data)
-    const { productId } = useParams();
-    const query = new URLSearchParams(useLocation().search);
-    const id = query.get('id');
-    const name = query.get('name');
-    const description = query.get('description');
-    const price = query.get('price');
-    const amount = query .get('amount');
-
-    const product = {
-        id,
-        name: decodeURIComponent(name),
-        description: decodeURIComponent(description),
-        price,
-        amount: decodeURIComponent(amount)
-    }
 
     const cartCTX = useContext(CartContext);
 
-    const [isFetching, setIsFetching] = useState(false);
-    const [error, setError] = useState();
-
-    useEffect(() => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        async function fetchCartProduct() {
-            setIsFetching(true);
-
-            try {
-                const products = await axios.get(`${apiUrl}/api/cart-products`);
-                
-                cartCTX.setCart({ items: products.data.rows, totalAmount: +products.data.rows[ products.data.rows.length -1 ].totalAmount });
-            } catch (error) {
-                if (product.id) {
-                    setError();
-                } else {
-                    setError({ message: "Failed to fetch cart products." });
-                }
-            }
-
-            setIsFetching(false);
-        }
-
-          fetchCartProduct();
-    }, []);
+    cartCTX.setCart({ items: data.carts, totalAmount: +data.carts[ data.carts.length -1 ].totalAmount });
 
     return (
         <div>
-            {isFetching && <LoadingSpinner />}
-            {error && <Error title='An Error occurred!' body={error.message} />}
-            {!isFetching && !error && (
-                <Detail product={product} productId={productId} />)}
+            <Detail product={data.product} productId={data.productId} />
         </div>
     )
 }
@@ -70,8 +27,7 @@ export default DetailPage;
 export async function detailLoader({ request, params }) {
     const url = new URL(request.url);
     const query = url.searchParams;
-    
-    //const id = params.productId;
+
     const id = query.get('id');
     const name = query.get('name');
     const description = query.get('description');
@@ -85,15 +41,18 @@ export async function detailLoader({ request, params }) {
         price,
         amount: decodeURIComponent(amount)
     }
-    
-    return product
-    // try {
-    //     const response = await axios.get(`${apiUrl}/api/products/products/${id}?id=${request.id}&name=${decodeURIComponent(request.name)}&description=${decodeURIComponent(request.description)}&price=${request.price}&amount=${decodeURIComponent(request.amount)}`);
-    //     return response.data.rows;
-    //   } catch (error) {
-    //     console.error('Error fetching data:', error);
-    //     throw json( { isNextLine: false, message: 'Failed to fetch details for selected product.' }, {
-    //       status: 500
-    //     } )
-    // }
+
+    try {
+        const products = await axios.get(`${apiUrl}/api/cart-products`);
+        return { product, productId: params.productId, carts: products.data.rows };
+
+    } catch (error) {
+        if (product.id) {
+            return ;
+        } else {
+            throw json( { isNextLine: true, message: 'Failed to fetch cart products.' }, {
+                status: 500
+              } )
+        }
+    }
 }
