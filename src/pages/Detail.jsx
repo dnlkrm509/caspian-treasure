f9194d9487154b2dda988286b969ef3cd49692d9
Daniel <danialkarimi109@gmail.com>
2024-07-12 22:39:22 +0100

    update router

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -46,7 +46,7 @@ export async function detailLoader({ request, params }) {
     }
 
     try {
-        const products = await axios.get(`${apiUrl}/api/cart-productsss`);
+        const products = await axios.get(`${apiUrl}/api/cart-products`);
         return { product, productId: params.productId, carts: products.data.rows };
 
     } catch (error) {
