2d4eeed87d46cd5213d4ad6bdb9111f0114a9703
Daniel <danialkarimi109@gmail.com>
2024-07-12 15:09:03 +0100

    update router

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,24 +6,26 @@ import NavSmScreen from "../components/Nav/NavSmScreen";
 function ErrorPage() {
     const error = useRouteError();
     console.log(error)
-    const classes = error.data.isNextLine ? '' : 'flex flex-row gap-64';
-    const pClasses = error.data.isNextLine ? '' : 'mt-[2%] ml-[3%]';
+    let classes = '';
+    let pClasses = '';
 
     let button;
-    if (error.data.button) {
-        button = <button
-                    className="flex-none p-4 rounded-[20px] bg-amber-200 hover:bg-amber-300 active:bg-amber-400
-                        focus:outline-none focus:ring focus:ring-amber-100"
-                >
-                    Try again
-                </button>
-    }
-
 
     let title = 'An error occurred!';
     let message = 'Something went wrong!';
 
     if (error.status === 500) {
+        classes = error.data.isNextLine ? '' : 'flex flex-row gap-64';
+        pClasses = error.data.isNextLine ? '' : 'mt-[2%] ml-[3%]';
+    
+        if (error.data.button) {
+            button = <button
+                        className="flex-none p-4 rounded-[20px] bg-amber-200 hover:bg-amber-300 active:bg-amber-400
+                            focus:outline-none focus:ring focus:ring-amber-100"
+                    >
+                        Try again
+                    </button>
+        }
         message = error.data.message;
     }
 
