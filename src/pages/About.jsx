a620f95b191d1c16d6ed50112d37c463c4db3cd3
Danial <danial@email.com>
2024-07-15 01:54:35 +0100

    update routers to recieve cart products

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,29 +5,15 @@ import CartContext from "../store/cart-context.js";
 
 function AboutPage() {
     const data = useRouteLoaderData('root');
-    console.log(data);
     const cartCTX = useContext(CartContext);
-
-    const [isFetching, setIsFetching] = useState(false);
-    const [error, setError] = useState();
 
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
-        async function fetchCartProduct() {
-            setIsFetching(true);
+        cartCTX.setCart({
+            items: data,
+            totalAmount: +data[ data.length -1 ].totalAmount
+        });
 
-            try {
-                const products = await axios.get(`${apiUrl}/api/cart-products`);
-                
-                cartCTX.setCart({ items: products.data.rows, totalAmount: +products.data.rows[ products.data.rows.length -1 ].totalAmount });
-            } catch (error) {
-                setError({ message: "Failed to fetch cart products." });
-            }
-
-            setIsFetching(false);
-        }
-
-        fetchCartProduct();
     }, [])
 
     return (
