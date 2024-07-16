9f95158c9395a510277383c1817824bbe4d46689
Danial <danial@email.com>
2024-07-16 22:10:46 +0100

    correct requests to server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,8 +36,8 @@ function HomePage(props) {
           await axios.post(`${apiUrl}/api/users`, {
             name:'1', password:'1', email:'1', address:'1', city:'1', state:'1',zip:'1',country:'1'
           })
+          await axios.post(`${apiUrl}/api/cart-products`, { newProduct: [], userId: 11, totalAmount: '0.00' } );
         }
-        await axios.post(`${apiUrl}/api/cart-products`, { newProduct: [], userId: 11, totalAmount: '0.00' } );
 
         const carts = await axios.get(`${apiUrl}/api/cart-products`);
         console.log(carts.data.rows)
