1138f4df185bb47febd354e74bef03fffdac070d
Danial <danial@email.com>
2024-07-21 10:06:33 +0100

    correct get requests to cart products endpoint in server

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -65,6 +65,8 @@ export async function loader() {
     // Get the last user ID from the users list
     const userId = users.data.rows[users.data.rows.length - 1].id;
 
+    console.log('user id:', userId)
+
     // Fetch cart products for the last user
     let cartResponse = await axios.get(`${apiUrl}/api/cart-products/${userId}`);
 
